ce2c4f71df8dc91620146870f5e2df7f1f60d0c9
sureshnammi@gmail.com <TestUser@gmail.com>
2019-03-31 18:45:36 +1100

    Added Read Me
    Added TestExecution.bat
    Added ScheduleTestExecution.bat

---
+++ ScheduleTestExecution.bat
@@ -0,0 +1,2 @@
+schtasks /create /sc minute /mo 60 /tn "WeatherAPIScheduleExecution" /tr C:\testgit1\APITestingRepo\TestExecution.bat
+
---
+++ TestExecution.bat
@@ -0,0 +1,4 @@
+@set Path=C:\testgit1
+cd %Path%\APITestingRepo\WeatherAPI\packages\NUnit.ConsoleRunner.3.10.0\tools
+
+nunit3-console %Path%\APITestingRepo\WeatherAPI\WeatherAPI\APITesting.csproj
